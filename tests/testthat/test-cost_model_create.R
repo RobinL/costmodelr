a6c943919138081466b03285eaf4b386f9224428
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2018-12-16 10:01:43 +0000

    fixed unit tests on R 3.4.2 and made Shiny parts optional to reduce dependencies

--- tests/testthat/test-cost_model_create.R
+++ tests/testthat/test-cost_model_create.R
@@ -1,11 +1,12 @@
 context("Test cost model factory")
 test_that("Test cost model factory", {
 
+  # This is required due to https://stackoverflow.com/a/47467017/1779128
+  Sys.setenv(TZ = "Europe/London")
+
   key_dates <- readr::read_csv(system.file("extdata", "key_dates_exceldates.csv", package="costmodelr"), col_types=readr::cols())
   cm <- costmodelr::create_cost_model(key_dates)
 
   testthat::expect_equal(cm$key_dates$date, as.Date(c("2017-01-01", "2017-05-01", "2017-10-01", "2018-05-01", "2018-10-01", "2019-01-01")))
 
 })
-
-
